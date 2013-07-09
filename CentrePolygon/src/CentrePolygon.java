5c5ed8d357440e994b5b7c1b66d085df793a978b
AHoran <adam.horan@AVT102293.melb.avtech.com.au>
2013-07-10 00:23:37 +1000

    Add test classes

--- CentrePolygon/src/CentrePolygon.java
+++ CentrePolygon/src/CentrePolygon.java
@@ -2,6 +2,7 @@
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.geom.Line2D;
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -13,57 +14,68 @@ public class CentrePolygon {
     public static Point findCentre(Polygon targetPolygon) {
         Point PIA = new Point();
         double currentAccuracy = Double.MAX_VALUE;
-        double minimumAccuracy = 0.0;
+        double minimumAccuracy = 2.0;
         double minDist = Double.MAX_VALUE;
         double maxMinDist = 0;
         Point minBound = PolyUtils.getMin(targetPolygon);
         Point maxBound = PolyUtils.getMax(targetPolygon);
-        int currentx, currenty;
+        double currentx, currenty;
         Point currentNode;
         int misses = 0;
-        int missLimit = 10;
+        int missLimit = 40;
+        System.out.println(String.format("Initial bounds min: %1$s, %2$s max: %3$s, %4$s", minBound.getX(),minBound.getY(),maxBound.getX(),maxBound.getY()));
+        new Random().setSeed(new Date().getTime());
         while (currentAccuracy > minimumAccuracy) {
+            misses = 0;
             while (misses < missLimit) {
-                currentx = new Random().nextInt(maxBound.x - minBound.x) + minBound.x;
+                System.out.println(String.format("Misses: %1$s, miss Limit: %2$s", misses, missLimit));
+
+                currentx = (new Random().nextDouble() * (maxBound.x - minBound.x)) + minBound.x;
                 currenty = new Random().nextInt(maxBound.y - minBound.y) + minBound.y;
-                currentNode = new Point(currentx, currenty);
+                currentNode = new Point();
+                currentNode.setLocation(currentx, currenty);
+                System.out.println(String.format("New Test point: %1$s, %2$s", currentx, currenty));
                 if (targetPolygon.contains(currentNode)) {
+                    minDist = Double.MAX_VALUE;
                     for (int i = 0; i < targetPolygon.npoints; i++) {
                         double dist;
-                        if(i == targetPolygon.npoints - 1){
-                            dist = Line2D.ptLineDist((double)targetPolygon.xpoints[i], (double)targetPolygon.ypoints[i], (double)targetPolygon.xpoints[0], (double)targetPolygon.ypoints[0], (double)currentx, (double)currenty);
-                        }else{
-                            dist = Line2D.ptLineDist((double)targetPolygon.xpoints[i], (double)targetPolygon.ypoints[i], (double)targetPolygon.xpoints[i+1], (double)targetPolygon.ypoints[i+1], (double)currentx, (double)currenty);
+                        if (i == targetPolygon.npoints - 1) {
+                            dist = Line2D.ptLineDist((double) targetPolygon.xpoints[i], (double) targetPolygon.ypoints[i], (double) targetPolygon.xpoints[0], (double) targetPolygon.ypoints[0], currentx, currenty);
+                        } else {
+                            dist = Line2D.ptLineDist((double) targetPolygon.xpoints[i], (double) targetPolygon.ypoints[i], (double) targetPolygon.xpoints[i + 1], (double) targetPolygon.ypoints[i + 1], currentx, currenty);
                         }
-                        if(dist < minDist){
+                        
+                        if (dist < minDist) {
                             minDist = dist;
-                            PIA = currentNode;
                         }
+                        System.out.println(String.format("Distance to line: %1$s. Current min dist: %2$s", dist, minDist));
+
                     }
-                    
-                    if(minDist > maxMinDist){
+                    if (minDist > maxMinDist) {
                         maxMinDist = minDist;
+                        PIA = currentNode;
+                        System.out.println(String.format("New PIA found %1$s, %2$s. Max min dist %3$s", PIA.getX(), PIA.getY(),maxMinDist));
                         misses = 0;
-                    }else{
-                        misses ++;
+                    } else {
+                        misses++;
                     }
+
+
+                } else {
+                    System.out.println("Test point not in poly");
                 }
             }
             currentAccuracy = Math.min(maxBound.x - minBound.x, maxBound.y - minBound.x);
-            //    # update the bounds of the region
-//    min_x := PIA(x) - (max_x - min_x) / (sqrt(2) * 2)
-//    max_x := PIA(x) + (max_x - min_x) / (sqrt(2) * 2)
-//    min_y := PIA(y) - (max_y - min_y) / (sqrt(2) * 2)
-//    max_y := PIA(y) + (max_y - min_y) / (sqrt(2) * 2)
+            System.out.println(String.format("Current Accuracy %1$s", currentAccuracy));
             double factor = Math.sqrt(2.0) * 2.0;
-            int minBound_x = (int)(PIA.x - (maxBound.x - minBound.x) / factor);
-            int maxBound_x = (int)(PIA.x + (maxBound.x - minBound.x) / factor);
-            int minBound_y = (int)(PIA.y - (maxBound.y - minBound.y) / factor);
-            int maxBound_y = (int)(PIA.y + (maxBound.y - minBound.y) / factor);
-            
-            minBound = new Point(minBound_x, minBound_y);
-            maxBound = new Point(maxBound_x, maxBound_y);
-                    
+            double minBound_x =  (PIA.x - ((maxBound.x - minBound.x) / factor));
+            double maxBound_x =  (PIA.x + ((maxBound.x - minBound.x) / factor));
+            double minBound_y =  (PIA.y - ((maxBound.y - minBound.y) / factor));
+            double maxBound_y =  (PIA.y + ((maxBound.y - minBound.y) / factor));
+
+            minBound.setLocation(minBound_x, minBound_y );
+            maxBound.setLocation(maxBound_x, maxBound_y);
+            System.out.println(String.format("New Bounds min: %1$s, %2$s max: %3$s, %4$s", minBound_x, minBound_y, maxBound_x, maxBound_y));
         }
 
 //    while accuracy > minimum_accuracy do:
--- CentrePolygon/test/CentrePolygonTest.java
+++ CentrePolygon/test/CentrePolygonTest.java
@@ -3,6 +3,9 @@
  * and open the template in the editor.
  */
 
+import java.awt.Point;
+import java.awt.Polygon;
+import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,6 +19,51 @@ public class CentrePolygonTest {
     }
 
     @Test
-    public void testFindCentre() {
+    public void testFindCentreTriangle1() {
+        ArrayList<Point> points = new ArrayList<Point>();
+        points.add(new Point(10,10));
+        points.add(new Point(30,30));
+        points.add(new Point(30,10));
+        Polygon poly = PolyUtils.CreatePolygonFromList(points);
+        Point findCentre = CentrePolygon.findCentre(poly);
+        
+        assertEquals("Incentre should be 24.1,15.9", 24.1, findCentre.getX(), 1);
+        assertEquals("Incentre should be 24.1,15.9", 15.9, findCentre.getY(), 1);
+        
     }
+    
+    //Test results from wolframalpha searching on "Incenter  {25,62}, {346,14}, {43,146}"
+     @Test
+    public void testFindCentreTriangle2() {
+        ArrayList<Point> points = new ArrayList<Point>();
+        points.add(new Point(25,62));
+        points.add(new Point(346,14));
+        points.add(new Point(43,146));
+        Polygon poly = PolyUtils.CreatePolygonFromList(points);
+        Point findCentre = CentrePolygon.findCentre(poly);
+        
+        assertEquals(70.1002, findCentre.getX(), 1);
+        assertEquals( 93.2293, findCentre.getY(), 1);
+        
+    }
+     
+     //rectangles are hard, as there's a line that describes the max min distance from any edge...
+     //and we might find any point on that line
+     @Test
+     public void testSimpleRectangle(){
+          ArrayList<Point> points = new ArrayList<Point>();
+        points.add(new Point(-100,-100));
+        points.add(new Point(200,-100));
+        points.add(new Point(200,50));
+        points.add(new Point(-100,50));
+        Polygon poly = PolyUtils.CreatePolygonFromList(points);
+        Point findCentre = CentrePolygon.findCentre(poly);
+        
+        assertEquals(-25.0, findCentre.getY(), 1);
+        //we can't assert an X for this rectangle
+        //assertEquals(50.0, findCentre.getX(), 1);
+        
+     }
+     
+   
 }
