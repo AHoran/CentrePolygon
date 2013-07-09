6464b97c8aca6ab98281747cc20e2348ea3b56ba
AHoran <adam.horan@AVT102293.melb.avtech.com.au>
2013-07-09 22:49:14 +1000

    Add test classes

---
+++ CentrePolygon/test/CentrePolygonTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author adam.horan
+ */
+public class CentrePolygonTest {
+    
+    public CentrePolygonTest() {
+    }
+
+    @Test
+    public void testFindCentre() {
+    }
+}
